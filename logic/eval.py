2a9df358b27ad67fd780634fdc3c89889eb3c22d
LachyCastro <74092172+LachyCastro@users.noreply.github.com>
2022-12-21 06:53:49 -0500

    Update eval.py

--- logic/eval.py
+++ logic/eval.py
@@ -2,24 +2,48 @@ import matplotlib.pyplot as plt
 
 
 def build_graphicss(results):
-    ap = []
-    setr = []
+    p = []
+    r = []
     setf = []
     for i, _ in enumerate(results):
         if i % 3 == 0:
-            ap.append(results[i].value)
+            p.append(results[i].value)
         if i % 3 == 1:
-            setr.append(results[i].value)
+            r.append(results[i].value)
         if i % 3 == 2:
             setf.append(results[i].value)
-    return ap, setr, setf
+    measures = {'P': p, 'R': r, 'SetF': setf}
+    return measures
 
 
-def draw(ap, setr, setf, num_querys):
+def draw_P(num_querys, measures):
+    # plt.figure(figsize=(15, 7))
     fig, ax = plt.subplots()
     querys = [i for i in range(num_querys + 1)]
-    measures = {'AP': ap, 'SetR': setr, 'SetF': setf}
-    ax.plot(querys, measures['AP'], color='tab:purple')
-    ax.plot(querys, measures['SetR'], color='tab:green')
-    ax.plot(querys, measures['SetF'], color='tab:cyan')
+    ax.plot(querys, measures['P'], color='tab:purple')
+    plt.title("Precisión")
+    plt.xlabel('Querys')
+    plt.ylabel('Value')
+    plt.show()
+
+
+def draw_R(num_querys, measures):
+    # plt.figure(figsize=(15, 7))
+    fig, ax = plt.subplots()
+    querys = [i for i in range(num_querys + 1)]
+    ax.plot(querys, measures['R'], color='tab:purple')
+    plt.title("Recobrado")
+    plt.xlabel('Querys')
+    plt.ylabel('Value')
+    plt.show()
+
+
+def draw_setF(num_querys, measures):
+    # plt.figure(figsize=(15, 7))
+    fig, ax = plt.subplots()
+    querys = [i for i in range(num_querys + 1)]
+    ax.plot(querys, measures['SetF'], color='tab:purple')
+    plt.title("Medida F")
+    plt.xlabel('Querys')
+    plt.ylabel('Value')
     plt.show()
